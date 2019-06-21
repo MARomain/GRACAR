51faf4debb7ac1d830b219a2ab2d4c03180092f8
Robin <35851348+MultiCut@users.noreply.github.com>
2019-06-21 16:48:46 +0200

    anim bateau a finir

---
+++ ProjectGracarV2/Assets/Assets/Environment/Boat/Top_Down.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Top_Down : MonoBehaviour
+{
+
+
+
+    Animator Animator;
+
+    public bool TopDown;
+
+    void Start()
+    {
+
+        Animator = gameObject.GetComponent<Animator>();
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (TopDown) Animator.SetBool("TopDown", true);
+
+        if (!TopDown) Animator.SetBool("TopDown", false);
+
+    }
+
+}
---
+++ ProjectGracarV2/Assets/Scripts/Player_Attach.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Attach : MonoBehaviour
+{
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.transform.parent.parent = transform;
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.parent.parent = transform;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.parent.parent = null;
+        }
+
+    }
+
+
+
+}
