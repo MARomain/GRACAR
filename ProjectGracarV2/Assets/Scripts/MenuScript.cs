04e108642f269fc20c7569da1d27da651f93bd3d
MARomain <33660355+MARomain@users.noreply.github.com>
2019-06-27 17:45:10 +0200

    Menu working

---
+++ ProjectGracarV2/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public float delayBeforeKeyPress;
+    public bool canPress = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delayBeforeKeyPress -= Time.deltaTime;
+
+        if(delayBeforeKeyPress <= 0)
+        {
+            canPress = true;
+        }
+
+        if(canPress == true)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                if (Input.GetKeyDown("joystick 1 button " + i))
+                {
+                    SceneManager.LoadScene("LEVEL BATEAU BACKUP");
+                }
+            }
+
+            if (Input.anyKeyDown)
+            {
+                SceneManager.LoadScene("LEVEL BATEAU BACKUP");
+            }
+        }
+
+        
+
+
+        //SceneManager.LoadScene("OtherSceneName");
+
+    }
+
+
+}
